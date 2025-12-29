9bf52243b7223d38bad8720c2df3a9b5fccad7bd
Jitin Singla <jitin2025@gmail.com>
2025-12-29 14:52:50 +0530

    Update pytorch_network.py

--- codes/pytorch_network.py
+++ codes/pytorch_network.py
@@ -2,21 +2,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.checkpoint as checkpoint
-#ConvolutionBlock
-
-# class ConvBlock(nn.Module):
-#     def __init__(self, in_channels, out_channels): 
-#         super(ConvBlock, self).__init__()
-#         self.conv1 = nn.Conv3d(in_channels, out_channels, 3, padding= "same")
-#         self.batchnorm1 = nn.BatchNorm3d(out_channels)
-#         self.relu1 = nn.ReLU()
-#         self.conv2 = nn.Conv3d(out_channels, out_channels, 3, padding = "same")
-#         self.batchnorm2 = nn.BatchNorm3d(out_channels)
-#         self.relu2 = nn.ReLU()
-#     def forward(self, inputs):
-#         x = self.relu1(self.batchnorm1(self.conv1(inputs)))
-#         x = self.relu2(self.batchnorm2(self.conv2(x)))    
-#         return x;
 
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -36,9 +21,7 @@ class ConvBlock(nn.Module):
         x = self.batchnorm2(x)
         x = self.relu2(x)    
         return x;
-    
-#EncoderBlock
-    
+  
 class EncoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(EncoderBlock, self).__init__()
@@ -49,10 +32,7 @@ class EncoderBlock(nn.Module):
         x = self.conv_block(inputs)
         p = self.maxpool(x)
         return x,p;
-    
-    
-#DecoderBlock
-    
+   
 class DecoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DecoderBlock, self).__init__()
@@ -64,17 +44,13 @@ class DecoderBlock(nn.Module):
         x = torch.cat([x, skip_connection], axis = 1)
         x = self.conv_block(x)
         return x;     
-    
-    
-#Unetwork Architecture 
-    
+
+#--------------- UNet Smaller variant------------------
 class UNet(nn.Module):
     def __init__(self, num_classes, input_shape = (1,64,704,704)):
-        super(UNet, self).__init__()
-        
+        super(UNet, self).__init__()        
         self.input_shape = input_shape
         self.num_classes = num_classes
-
         self.encoder1 = EncoderBlock(1,8)           
         self.encoder2 = EncoderBlock(8,16) 
         self.encoder3 = EncoderBlock(16,32)
@@ -84,16 +60,9 @@ class UNet(nn.Module):
         self.decoder3 = DecoderBlock(64,32)
         self.decoder2 = DecoderBlock(32,16)
         self.decoder1 = DecoderBlock(16,8)
-        self.output_conv = nn.Conv3d(8, num_classes, kernel_size=1 , padding = "same")
-        
-        # if num_classes == 1:
-        #     self.activation = nn.Sigmoid() 
-        # else:
-        #     self.activation = nn.Softmax(dim=1)   
-         
+        self.output_conv = nn.Conv3d(8, num_classes, kernel_size=1 , padding = "same")    
         self.activation = nn.Sigmoid()
-        
-
+    
     def forward(self, x):   
         s1, p1 = self.encoder1(x) 
         s2, p2 = self.encoder2(p1)
@@ -106,16 +75,14 @@ class UNet(nn.Module):
         d1 = self.decoder1(d2,s1)
         output = self.output_conv(d1)
         return output
-    
+
+#--------------- UNet Larger variant------------------
 class UNetDeep(nn.Module):
     def __init__(self, num_classes, input_shape = (1,64,704,704)):
         super(UNetDeep, self).__init__()
-        
         self.input_shape = input_shape
-        self.num_classes = num_classes
-        
-        unit = 16
-        
+        self.num_classes = num_classes    
+        unit = 16        
         self.encoder1 = EncoderBlock(1,unit)         # 1 kernels   
         self.encoder2 = EncoderBlock(unit,2*unit)    # 16 kernels
         self.encoder3 = EncoderBlock(2*unit,4*unit)  # 32 kernels
@@ -128,7 +95,6 @@ class UNetDeep(nn.Module):
         self.decoder2 = DecoderBlock(unit*4,unit*2)
         self.decoder1 = DecoderBlock(unit*2,unit)
         self.output_conv = nn.Conv3d(unit, num_classes, kernel_size=1 , padding = "same")
-        
         self.activation = nn.Sigmoid()
 
     def forward(self, x):   
