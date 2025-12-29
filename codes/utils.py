6eaf66068623764ed7c4961b3141b9ec12d00540
Jitin Singla <jitin2025@gmail.com>
2025-12-29 14:50:27 +0530

    Update utils.py

--- codes/utils.py
+++ codes/utils.py
@@ -3,6 +3,8 @@ import os, shutil, random, skimage
 from read_write_mrc import read_mrc, write_mrc
 from patchify import patchify, unpatchify
 from scipy import ndimage
+import struct
+import cupy as cp
 
 #----------Preprocessing----------
 def normalisation(img):
@@ -118,3 +120,49 @@ def Precision_recall_f1_score(prediction, Ground_Truth):
     recall = true_positive / (true_positive + false_negative)
     f1_score = 2 * (precision * recall) / (precision + recall)
     return precision, recall, f1_score 
+
+#----------Read Write MRC----------
+def read_mrc(filename):
+    filename = str(filename)
+    input_image=open(filename,'rb')
+    num_ints=56
+    sizeof_int=4
+    nlines_header=10
+    num_chars=80
+    head1=input_image.read(num_ints*sizeof_int) # read 56 long ints
+    head2=input_image.read(nlines_header*num_chars) #read 10 lines of 80 chars
+    byte_pattern='=' + 'l'*num_ints   #'=' required to get machine independent standard size
+    dim=struct.unpack(byte_pattern, head1)[:3][::-1]
+    imagetype=struct.unpack(byte_pattern,head1)[3]  #0: 8-bit signed, 1:16-bit signed, 2: 32-bit float, 6: unsigned 16-bit (non-std)
+    #print("Imagetype = {}".format(imagetype ))
+    if (imagetype == 0):
+        imtype='b'
+    elif (imagetype ==1):
+        imtype='h'
+    elif (imagetype ==2):
+        imtype='f4'
+    elif (imagetype ==6):
+        imtype='H'
+    else:
+        type='unknown'   #should put a fail here
+    num_voxels=dim[0]*dim[1]*dim[2]
+    image_data=cp.fromfile(file=input_image,dtype=imtype,count=num_voxels).reshape(dim)
+    input_image.close()
+    return image_data
+
+def write_mrc(filename, im, num_ints=56, sizeof_int=4, num_chars=800):
+    filename = str(filename)
+    type_modes = {
+        'b': 0,
+        'h': 1,
+        'f': 2,
+        'H': 6
+    }
+    mode = type_modes[im.dtype.char]
+    dims = im.shape[::-1]
+    header1 = struct.pack('=' + 'l'*num_ints, *(dims + (mode,) + (0,)*(num_ints - len(dims) - 1)))
+    header2 = struct.pack('=' + 'c'*num_chars, *(b" ",)*num_chars)
+    with open(filename, 'wb') as f:
+        f.write(header1)
+        f.write(header2)
+        f.write(im.tobytes())
