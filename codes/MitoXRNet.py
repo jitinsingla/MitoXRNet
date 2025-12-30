8c791f2d5a784acbe052e47d6e43cea9eebb8fa4
arun yadav <80715100+Arun07062000@users.noreply.github.com>
2025-12-31 01:41:59 +0530

    Clean up MitoXRNet.py by removing commented code
    Removed commented-out code for ConvBlock and UNetDeep classes, and cleaned up formatting.

--- codes/MitoXRNet.py
+++ codes/MitoXRNet.py
@@ -2,22 +2,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.checkpoint as checkpoint
+
 #ConvolutionBlock
-
-# class ConvBlock(nn.Module):
-#     def __init__(self, in_channels, out_channels): 
-#         super(ConvBlock, self).__init__()
-#         self.conv1 = nn.Conv3d(in_channels, out_channels, 3, padding= "same")
-#         self.batchnorm1 = nn.BatchNorm3d(out_channels)
-#         self.relu1 = nn.ReLU()
-#         self.conv2 = nn.Conv3d(out_channels, out_channels, 3, padding = "same")
-#         self.batchnorm2 = nn.BatchNorm3d(out_channels)
-#         self.relu2 = nn.ReLU()
-#     def forward(self, inputs):
-#         x = self.relu1(self.batchnorm1(self.conv1(inputs)))
-#         x = self.relu2(self.batchnorm2(self.conv2(x)))    
-#         return x;
-
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(ConvBlock, self).__init__()
@@ -38,7 +24,6 @@ class ConvBlock(nn.Module):
         return x;
     
 #EncoderBlock
-    
 class EncoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(EncoderBlock, self).__init__()
@@ -50,9 +35,7 @@ class EncoderBlock(nn.Module):
         p = self.maxpool(x)
         return x,p;
     
-    
 #DecoderBlock
-    
 class DecoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DecoderBlock, self).__init__()
@@ -65,9 +48,7 @@ class DecoderBlock(nn.Module):
         x = self.conv_block(x)
         return x;     
     
-    
 #Unetwork Architecture 
-    
 class UNet(nn.Module):
     def __init__(self, num_classes, input_shape = (1,64,704,704)):
         super(UNet, self).__init__()
@@ -85,15 +66,8 @@ class UNet(nn.Module):
         self.decoder2 = DecoderBlock(32,16)
         self.decoder1 = DecoderBlock(16,8)
         self.output_conv = nn.Conv3d(8, num_classes, kernel_size=1 , padding = "same")
-        
-        # if num_classes == 1:
-        #     self.activation = nn.Sigmoid() 
-        # else:
-        #     self.activation = nn.Softmax(dim=1)   
-         
         self.activation = nn.Sigmoid()
         
-
     def forward(self, x):   
         s1, p1 = self.encoder1(x) 
         s2, p2 = self.encoder2(p1)
@@ -105,53 +79,7 @@ class UNet(nn.Module):
         d2 = self.decoder2(d3,s2)
         d1 = self.decoder1(d2,s1)
         output = self.output_conv(d1)
-        # output = self.activation(output)       
         return output
-
-    
-# class UNetDeep(nn.Module):
-#     def __init__(self, num_classes, input_shape = (1,64,704,704)):
-#         super(UNetDeep, self).__init__()
-        
-#         self.input_shape = input_shape
-#         self.num_classes = num_classes
-
-#         self.encoder1 = EncoderBlock(1,8)           
-#         self.encoder2 = EncoderBlock(8,16) 
-#         self.encoder3 = EncoderBlock(16,32)
-#         self.encoder4 = EncoderBlock(32,64)
-#         self.encoder5 = EncoderBlock(64,128)
-#         self.bottleneck = ConvBlock(128,256)
-#         self.decoder5 = DecoderBlock(256,128)
-#         self.decoder4 = DecoderBlock(128,64)
-#         self.decoder3 = DecoderBlock(64,32)
-#         self.decoder2 = DecoderBlock(32,16)
-#         self.decoder1 = DecoderBlock(16,8)
-#         self.output_conv = nn.Conv3d(8, num_classes, kernel_size=1 , padding = "same")
-        
-#         # if num_classes == 1:
-#         #     self.activation = nn.Sigmoid() 
-#         # else:
-#         #     self.activation = nn.Softmax(dim=1)   
-         
-#         self.activation = nn.Sigmoid()
-        
-
-#     def forward(self, x):   
-#         s1, p1 = self.encoder1(x) 
-#         s2, p2 = self.encoder2(p1)
-#         s3, p3 = self.encoder3(p2)
-#         s4, p4 = self.encoder4(p3)
-#         s5, p5 = self.encoder5(p4)
-#         b1 = self.bottleneck(p5)
-#         d5 = self.decoder5(b1,s5)
-#         d4 = self.decoder4(d5,s4)
-#         d3 = self.decoder3(d4,s3)
-#         d2 = self.decoder2(d3,s2)
-#         d1 = self.decoder1(d2,s1)
-#         output = self.output_conv(d1)
-#         # output = self.activation(output)       
-#         return output
 
 class UNetDeep(nn.Module):
     def __init__(self, num_classes, input_shape = (1,64,704,704)):
@@ -174,12 +102,6 @@ class UNetDeep(nn.Module):
         self.decoder2 = DecoderBlock(unit*4,unit*2)
         self.decoder1 = DecoderBlock(unit*2,unit)
         self.output_conv = nn.Conv3d(unit, num_classes, kernel_size=1 , padding = "same")
-        
-        # if num_classes == 1:
-        #     self.activation = nn.Sigmoid() 
-        # else:
-        #     self.activation = nn.Softmax(dim=1)   
-         
         self.activation = nn.Sigmoid()
 
     def forward(self, x):   
@@ -195,5 +117,4 @@ class UNetDeep(nn.Module):
         d2 = self.decoder2(d3,s2)
         d1 = self.decoder1(d2,s1)
         output = self.output_conv(d1)
-        # output = self.activation(output)       
         return output
