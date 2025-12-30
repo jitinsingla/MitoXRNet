0902ddddd4724e4b905eb99b5ff3351ec87697bf
arun yadav <80715100+Arun07062000@users.noreply.github.com>
2025-12-31 01:40:06 +0530

    Delete Notebooks directory

--- Notebooks/PathConfig.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-py_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../codes'))
-
-if py_folder not in sys.path:
-    sys.path.insert(0, py_folder)
-
-print(f"Added codes folder to sys.path: {py_folder}")
