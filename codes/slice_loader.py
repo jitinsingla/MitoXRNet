ed968dad7c3851c514d641fb05ac646524455864
arun yadav <80715100+Arun07062000@users.noreply.github.com>
2026-01-01 13:11:33 +0530

    Refactor SliceLoader initialization and item retrieval
    Refactor SliceLoader to assert data_root_dir and remove unnecessary comments.

--- codes/slice_loader.py
+++ codes/slice_loader.py
@@ -8,31 +8,25 @@ from utils import *
     
 class SliceLoader(Dataset):
     def __init__(self, data_root_dir=None, images_dir="images", labels_dir="labels" ):
+        
         assert data_root_dir is not None, "Please pass the correct data_root_dir"
-
         images_list = sorted(glob.glob(os.path.join(data_root_dir, images_dir,'*.mrc')))
-            
-
         labels_list = sorted(glob.glob(os.path.join(data_root_dir, labels_dir,'*.mrc')))
-            
         assert len(labels_list) == len(images_list), "Lengths of labels and images should be the same."
-
         self.images_list = images_list
         self.labels_list = labels_list
         gc.collect()
 
     def __getitem__(self, item):
+        
         image = read_mrc(os.path.join(self.images_list[item]))
         image = np.expand_dims(image, axis=0).astype(np.float32)
-
         label = read_mrc(os.path.join(self.labels_list[item]))
-        
         label_nucleus = (label == 2.0).astype(np.float32)
         label_mito = (label == 5.0).astype(np.float32)
         label_combined = np.stack((label_nucleus,label_mito),0)
         image_tensor = torch.as_tensor(image)
         label_tensor = torch.as_tensor(label_combined)
-
         return image_tensor, label_tensor
     
     def __len__(self):
